8e58076391f2feb6ce549f2c37e2f0cdc5cd0837
alexandraalday <alexandra.alday@gmail.com>
2017-09-20 11:18:48 -0700

    hello rick

--- js/canvas.js
+++ js/canvas.js
@@ -1,9 +1,14 @@
 console.log('i am the canvas');
 
+let rick;
+
+
 function setup() {
 	createCanvas(400, 600);
+	rick = new Rick();
 }
 
 function draw() {
 	background(0);
+	rick.show();
 }
--- js/rick.js
+++ js/rick.js
@@ -1,1 +1,11 @@
 console.log('i am rick')
+
+function Rick() {
+	this.y = width/2;
+	this.x = 25;
+
+	this.show = function() {
+		fill(0, 255, 0);
+		ellipse(this.x, this.y, 16, 16);
+	}
+}
