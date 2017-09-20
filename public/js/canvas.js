7d5918a648c9b3fee7d1ec5db4286a76def1a3b1
alexandraalday <alexandra.alday@gmail.com>
2017-09-20 15:30:01 -0700

    user can now pause the game

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -2,7 +2,7 @@ console.log('i am the canvas');
 
 let rick;
 let pipes = [];
-
+let pause = false;
 
 function setup() {
 	createCanvas(800, 600);
@@ -11,35 +11,43 @@ function setup() {
 }
 
 function draw() {
-	background(0);
-	rick.update(); // falling action
-	rick.show(); // hello rick!
+	if (!pause){
+		background(0);
+		rick.update(); // falling action
+		rick.show(); // hello rick!
 
-	// infinite pipes
-	if (frameCount % 150 == 0){ // every x frames add a new set of pipes
-		pipes.push(new Pipe());
-	}
-	for (let i = pipes.length-1; i >= 0; i--){
-		pipes[i].show();
-		pipes[i].update();
-
-		if (pipes[i].hits(rick)) { // check for hits
-			console.log("OUCH!")
+		// infinite pipes
+		if (frameCount % 150 == 0){ // every x frames add a new set of pipes
+			pipes.push(new Pipe());
 		}
+		for (let i = pipes.length-1; i >= 0; i--){
+			pipes[i].show();
+			pipes[i].update();
 
-		if (pipes[i].disappear()){ // when the pipe disappears, remove it
-			pipes.splice(i, 1);
+			if (pipes[i].hits(rick)) { // check for hits
+				console.log("OUCH!")
+			}
+
+			if (pipes[i].disappear()){ // when the pipe disappears, remove it
+				pipes.splice(i, 1);
+			}
 		}
-	}
+	}	
 }
 
 function keyPressed() {
-	if (key == ' '){
+	if (key == ' ' && !pause){
 		console.log("SPACE")
 		rick.up();
 	}
-	if (keyCode == 38){
+	if (keyCode == 38 && !pause){
 		console.log("UP")
 		rick.up(); 
+	}
+	if (key == "p" || key == "P") {
+        pause = !pause;
+        if (pause) {
+ 			console.log('Skkrt skkrt')
+        }
 	}
 }
