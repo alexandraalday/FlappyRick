2176e85066daeddc673c04b6024183e7f8ede106
alexandraalday <alexandra.alday@gmail.com>
2017-09-20 12:11:46 -0700

    one set of pipes to avoid

--- js/canvas.js
+++ js/canvas.js
@@ -1,17 +1,24 @@
 console.log('i am the canvas');
 
 let rick;
+let pipes = [];
 
 
 function setup() {
 	createCanvas(400, 600);
 	rick = new Rick();
+	pipes.push(new Pipe()); // keep creating new pipes for rick to avoid
 }
 
 function draw() {
 	background(0);
 	rick.update(); // falling action
 	rick.show(); // hello rick!
+
+	for (let i = 0; i < pipes.length; i++){
+		pipes[i].show();
+		pipes[i].update();
+	}
 }
 
 function keyPressed() {
--- js/pipe.js
+++ js/pipe.js
@@ -1,1 +1,20 @@
 console.log('i am a pipe')
+
+function Pipe() {
+	this.top = random(height/2);
+	this.bottom = random (height/2);
+	this.x = width; // always start at edge of canvas
+	this.width = 20;
+	this.speed = 2;
+
+	// watch out for those pipes!
+	this.show = function() {
+		fill(0, 255, 0);
+		rect(this.x, 0, this.width, this.top); //top pipe
+		rect(this.x, (height - this.bottom), this.width, this.bottom); // bottom pipe
+	}
+
+	this.update = function() {
+		this.x -= this.speed;
+	}
+}
