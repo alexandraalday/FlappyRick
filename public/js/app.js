3a096447fef3ae055e63235c072d8ea3aa1fcbcd
alexandraalday <alexandra.alday@gmail.com>
2017-09-21 22:21:50 -0700

    user can log out and register

--- public/js/app.js
+++ public/js/app.js
@@ -7,11 +7,30 @@ const app = angular.module('flappyrick', []);
   		this.url = 'http://localhost:3000';
   		// users
 		this.user = {};
+		this.userPass = {};
+		this.showRegister = false;
+  		this.showLogin = false;
+  		this.loggedin = false;
+  		this.showProfile = false;
+  		this.showUpdate = false;
 
-		// user log in and set token in localStorage
+		// create new user
+		this.register = function(userReg){
+		    $http({
+		      method: 'POST',
+		      url: this.url + '/users',
+		      data: { user: {
+		        username: userReg.username,
+		        password: userReg.password
+		      }},
+		    }).then(function(response) {
+		      console.log(response);
+		      controller.login(userReg);
+		    })
+		  }
+
+		// user login 
 		this.login = function(userPass) {
-	    	console.log(userPass);
-
 	    	$http({
 		    	method: 'POST',
 		    	url: this.url + '/users/login',
@@ -20,14 +39,67 @@ const app = angular.module('flappyrick', []);
 		      	console.log(response);
 		     	this.user = response.data.user;
 		     	localStorage.setItem('token', JSON.stringify(response.data.token));
+		     	this.showLogin = false;
+      			this.showRegister = false;
+      			this.loggedin = true;
     		}.bind(this));
 
 	  	}
 
-	  	//user log out
+	  	// user logout
 		this.logout = function() {
 		  localStorage.clear('token');
+		  this.loggedin = false;
 		  location.reload();
+		}
+
+		// user update 
+		this.update = function(userUp) {
+	    	$http({
+		    	method: 'POST',
+		    	url: this.url + '/users/' + this.user.id,
+		    	data: { user: { username: userUp.username, password: userUp.password }},
+		    }).then(function(response) {
+		      	console.log(response);
+		     	this.user = response.data.user;
+    		}.bind(this));
+
+	  	}
+
+
+
+		// display/hide user forms
+		this.goRegister = function(){
+		  	this.showRegister = true;
+		    this.showLogin = false;
+		}
+		this.goLogin = function(){
+		    this.showLogin = true;
+		    this.showRegister = false;
+		}
+		this.goUpdate = function() {
+		    this.showUpdate = true;
+		}
+		this.registerForm = function(){
+		    if(this.showRegister) {
+		    }
+		    else {
+		      this.showRegister = !this.showRegister;
+		    }
+		}
+		this.loginForm = function(){
+		    if(this.showLogin){
+		    }
+		    else {
+		      this.showLogin = !this.showLogin;
+		    }
+		}
+		this.updateForm = function(){
+		    if(this.showUpdate) {
+		    }
+		    else {
+		      this.showUpdate = !this.showUpdate;
+		    }
 		}
 
 
